390adbf68654eed7d07460dba2c8b588c29cf58a
Jack Hardy <jackhardy1@gmail.com>
2016-09-23 11:26:33 +0100

    all files added

---
+++ voting-server/test/immutable_spec.js
@@ -0,0 +1,37 @@
+import {expect} from 'chai';
+import {List, Map} from 'immutable';
+
+describe('immutability', () => {
+
+  describe('A List', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      );
+    }
+
+    it('is immutable', () => {
+          let state = Map({
+            movies: List.of('Trainspotting', '28 Days Later')
+          });
+          let nextState = addMovie(state, 'Sunshine');
+
+          expect(nextState).to.equal(Map({
+            movies: List.of(
+              'Trainspotting',
+              '28 Days Later',
+              'Sunshine'
+            )
+          }));
+          expect(state).to.equal(Map({
+            movies: List.of(
+              'Trainspotting',
+              '28 Days Later'
+            )
+          }));
+    });
+  });
+
+});
