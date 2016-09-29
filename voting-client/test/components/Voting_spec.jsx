eb59bcc1f6fee3b91d1030335c8e1624aab1e3d9
Jack Hardy <jackhardy1@gmail.com>
2016-09-29 21:06:08 +0100

    once voted, button disabled

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {
 renderIntoDocument,
-scryRenderedDOMComponentsWithTag
+scryRenderedDOMComponentsWithTag,
+Simulate
 } from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
@@ -11,12 +12,38 @@ describe('Voting', () => {
 
   it('renders a pair of buttons', ()=> {
     const component = renderIntoDocument(
-      <Voting pair={["Trainspotting","28 days later"]} />
+      <Voting pair={["Trainspotting","28 days later"]}/>
     );
     const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Trainspotting');
     expect(buttons[1].textContent).to.equal('28 days later');
+  });
+
+  it('invokes a callback function when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={['Trainspotting', '28 days later']}
+          vote = {vote}/>
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
+  });
+
+  it('disables buttons when user has voted', () => {
+    const component = renderIntoDocument(
+      <Voting pair={['Trainspotting', '28 days later']}
+          hasVoted='Trainspotting' />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true);
   });
 
 });
